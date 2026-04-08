ef3992edcf28895cdbc9f4c28fe6e0283692ade6
a87chaud <95557076+a87chaud@users.noreply.github.com>
2026-04-08 02:41:35 -0400

    Add: reorganize-string - submission-2

---
+++ Data Structures & Algorithms/reorganize-string/submission-2.py
@@ -0,0 +1,36 @@
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        '''
+        {a: 0, b: 0 c: 0 d: 0}
+        curr element = c
+
+        d   b   c   a   d   c
+        '''
+        freqMap = Counter(s)
+        maxHeap = [(-val, key) for key,val in freqMap.items()]
+        heapq.heapify(maxHeap)
+        res = []
+        prev = ''
+
+        while maxHeap:
+            # take the most freq key in the map that is not prev
+            candidate = ''
+            maxFreq, maxStr = heapq.heappop(maxHeap)
+            if maxStr == prev and len(maxHeap) == 0:
+                return ''
+            
+            if maxStr == prev and len(maxHeap) > 0:
+                secondMax, secondStr = heapq.heappop(maxHeap)
+                candidate = secondStr
+                heapq.heappush(maxHeap, (maxFreq, maxStr))
+                if secondMax + 1 < 0:
+                    heapq.heappush(maxHeap, (secondMax + 1, secondStr))
+            else:
+                candidate = maxStr
+                if maxFreq + 1 < 0:
+                    heapq.heappush(maxHeap, (maxFreq + 1, maxStr))
+            if not candidate:
+                return ''
+            prev = candidate
+            res.append(candidate)
+        return ''.join(res)
